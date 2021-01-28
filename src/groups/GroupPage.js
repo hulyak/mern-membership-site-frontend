a9916eae1ad382ee8224dd1c3a342bd1b5fb3b78
hulya <hulyakarakayaa06@gmail.com>
2021-01-28 09:36:22 -0800

    create the accept and reject options for the owner

--- src/groups/GroupPage.js
+++ src/groups/GroupPage.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useUser } from '../auth';
-import { useProtectedResource } from '../data';
+import { postWithCredentials, useProtectedResource } from '../data';
+import { MessagesList } from '../messages';
+import { RequestsList } from '../requests';
 
 export const GroupPage = () => {
   const [messageValue, setMessageValue] = useState('');
@@ -16,10 +18,53 @@ export const GroupPage = () => {
     }
   );
 
+  const acceptRequest = async (requestId) => {
+    const response = await postWithCredentials(
+      `/groups/${id}/requests/${requestId}/accept`
+    );
+    const updatedRequest = await response.json();
+    // update the requests with the new request
+    setGroup({ ...group, requests: updatedRequest });
+  };
+
+  const rejectRequest = async (requestId) => {
+    const response = await postWithCredentials(
+      `/groups/${id}/requests/${requestId}/reject`
+    );
+    const updatedRequest = await response.json();
+    // update the requests with the deleted request
+    setGroup({ ...group, requests: updatedRequest });
+  };
+
+  // create new message
+  const postMessage = async () => {
+      const response = await postWithCredentials(`/groups/${id}/messages`, {text: messageValue});
+      const updatedMessages = await response.json();
+      setGroup({ ...group, messages: updatedMessages});
+      setMessageValue('')
+  }
+
   return (
     <div className="centered-container">
       <h1>{group.name}</h1>
       <p>Owned by: {group.owner.fullName}</p>
+      <MessagesList messages={group.messages} />
+      <div className="new-message-form">
+        <input
+          type="text"
+          placeholder="type your message here"
+          value={messageValue}
+          onChange={(e) => setMessageValue(e.target.value)}
+        />
+        <button onClick={postMessage}>Submit</button>
+      </div>
+      {group.ownerId === user.uid ? (
+        <RequestsList
+          requests={group.requests}
+          onAccept={acceptRequest}
+          onReject={rejectRequest}
+        />
+      ) : null}
     </div>
   );
 };
